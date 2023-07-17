308983c88ef2850e3827cdf6c41c239b6b3ea1f9
前井凉花 <59602343+maeisu@users.noreply.github.com>
2023-07-17 16:34:12 +0900

    save

--- philo/main.c
+++ philo/main.c
@@ -1,7 +1,120 @@
 #include "philo.h"
 
+// t_threaddata *data_init(int argc, char **argv)
+// {
+//     t_threaddata *data;
+//     t_threaddata *tmp;
+//     t_threaddata *first;
+//     int i;
+
+//     tmp = NULL;
+//     i = 0;
+//     while (i < argc)
+//     {
+//         data = malloc(sizeof(t_threaddata));
+//         pthread_mutex_init(&data->mutex, NULL);
+//         data->right = false;
+//         data->left = false;
+//         data->die = atoi(argv[2]);
+//         data->eat = atoi(argv[3]);
+//         data->sleep = atoi(argv[4]);
+//         data->status = THINK;
+//         if (tmp != NULL)
+//         {
+//             tmp->next = data;
+//             data->prev = tmp;
+//         }
+//         else
+//         {
+//             first = data;
+//             data->prev = NULL;
+//         }
+//         tmp = data;
+//         i++;
+//     }
+//     tmp->next = NULL;
+//     return (first);
+// }
+
+// void *thread(void *t_data)
+// {
+//     t_threaddata *data;
+
+//     data = (t_threaddata *)t_data;
+//     pthread_mutex_lock(&data->mutex);
+//     pthread_mutex_unlock(&data->mutex); 
+//     return(NULL);
+// }
+
+// void    philo(int argc, char **argv)
+// {
+//     t_threaddata *data;
+//     t_threaddata *tmp;
+//     int fork;
+
+//     fork = atoi(argv[1]);
+//     data = data_init(argc, argv);
+//     tmp = data;
+//     while (data)
+//     {
+//         pthread_create(&data->thread_id, NULL, thread, (void *)data);
+//         data = data->next;
+//     }
+//     data = tmp;
+//     while (data->next)
+//     {
+//         pthread_join(data->thread_id, NULL);
+//         data = data->next;
+//     }
+// }
+
+void philo_act()
+{
+    printf("test\n");
+}
+
 int main(int argc, char **argv)
 {
     for (int i=0; i < argc; i++)
-        printf("%s\n", argv[i]);
+        printf("%s ", argv[i]);
+    printf("\n");
+
+    if (argc != 5 && argc != 6)
+        return (1);
+
+    int fork = atoi(argv[1]);
+    int to_die = atoi(argv[2]);
+    int to_eat = atoi(argv[3]);
+    int to_sleep = atoi(argv[4]);
+    int max_eat = -1;
+    if (argv[5])
+        max_eat = atoi(argv[5]);
+    if (to_die < (to_eat + to_sleep))
+        return (1);
+    pthread_mutex_t *fork_mutex;
+    fork_mutex = malloc(sizeof(fork_mutex) * fork);
+    if (!fork_mutex)
+        return (1);
+    t_philo *philo;
+    philo = malloc(sizeof(t_philo) * fork);
+    if (!philo)
+        return (1);
+    int i = 0;
+    while (i < fork)
+    {
+        pthread_mutex_init(&fork_mutex[i++], NULL);
+        philo[i].r_fork = i;
+        philo[i].l_fork = (i + 1) % fork;
+        philo[i].meal_count = 0;
+    }
+    i = 0;
+    while (i < fork)
+    {
+        pthread_create(&philo[i++].thread_id, NULL, (void *)philo_act, NULL);
+    }
+    i = 0;
+    while (i < fork)
+    {
+        pthread_join(philo[i++].thread_id, NULL);
+    }
 }
--- philo/philo.h
+++ philo/philo.h
@@ -7,5 +7,38 @@
 #include <unistd.h>
 #include <sys/time.h>
 #include <pthread.h>
+#include <stdbool.h>
+
+// struct s_philo
+// {
+//     enum status;
+
+// };
+
+typedef enum s_status{
+    SLEEP,
+    EAT,
+    THINK,
+}           t_status;
+
+typedef struct s_data
+{
+    int time_eat;
+    int time_sleep;
+    int time_die;
+
+}   t_data;
+
+typedef struct s_philo
+{
+    pthread_t id;
+    // pthread_mutex_t mutex;
+    pthread_t thread_id;
+    int r_fork;
+    int l_fork;
+    int meal_count;
+
+}   t_philo;
+
 
 #endif
